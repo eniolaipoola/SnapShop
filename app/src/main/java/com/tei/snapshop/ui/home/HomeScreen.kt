3a0942a30229e169b4cf410b2b58c59d8e8aa8d1
Eniola Ipoola <eniolaipoola@gmail.com>
2024-07-02 17:34:34 +0100

    fix: remove default test files from project

--- app/src/main/java/com/tei/snapshop/ui/home/HomeScreen.kt
+++ app/src/main/java/com/tei/snapshop/ui/home/HomeScreen.kt
@@ -1,14 +1,14 @@
-package com.tei.snapshop.ui.feature_onboarding
+package com.tei.snapshop.ui.home
 
 import androidx.compose.runtime.Composable
 
 /**
  * Class Description
- * Created by Eniola Ipoola on 05/06/2024.
+ * Created by Eniola Ipoola on 26/06/2024.
  * Copyright (c). All rights reserved
  */
 
 @Composable
-fun OnboardingPage(
-){
+fun HomeScreen() {
+    
 }
